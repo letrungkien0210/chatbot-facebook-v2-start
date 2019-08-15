cbf303e93879f334ba509ff29188bb6988e28067
Kien Le <letrungkien0210@gmail.com>
2019-08-15 22:55:22 +0700

    Applied the Sendgrid

--- config.js
+++ config.js
@@ -2,9 +2,14 @@ module.exports = {
     FB_PAGE_TOKEN: process.env.FB_PAGE_TOKEN ,
     FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
     FB_APP_SECRET: process.env.FB_APP_SECRET,
-    SERVER_URL: process.env.SERVER_URL,
     GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
     DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
-    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY,
+    SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
+    SENDGRID_USERNAME: process.env.SENDGRID_USERNAME,
+    SERVER_URL: process.env.SERVER_URL,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_TO: process.env.EMAIL_TO,
+    SENGRID_API_KEY: process.env.SENGRID_API_KEY,
 };
